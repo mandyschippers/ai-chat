b53460c73470cb1c9b590db589c6f56da42406fb
mandyschippers <mandyschippers@hotmail.com>
2023-03-13 16:55:13 +1100

    todos

--- frontend/src/components/Book.js
+++ frontend/src/components/Book.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { BASE_URL } from "../constants";
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+} from "@chatscope/chat-ui-kit-react";
 import axios from "axios";
 
 const Book = (props) => {
@@ -11,6 +19,8 @@ const Book = (props) => {
     console.log("data", response.data);
   };
 
+  //TODO: fetch book by handle and initialise conversation with character
+
   useEffect(() => {
     fetchBookByHandle(props.handle);
   }, []);
@@ -18,6 +28,22 @@ const Book = (props) => {
   return (
     <div>
       <h1>Book {props.handle}</h1>
+      <div style={{ position: "relative", height: "500px" }}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList>
+              <Message
+                model={{
+                  message: "Hello my friend",
+                  sentTime: "just now",
+                  sender: "Joe",
+                }}
+              />
+            </MessageList>
+            <MessageInput placeholder="Type message here" />
+          </ChatContainer>
+        </MainContainer>
+      </div>
     </div>
   );
 };
